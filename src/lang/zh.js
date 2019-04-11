f59e6b26f7706ddb2dbe4010de557e7466a59ca5
CuiYang16 <cuiyangmail@163.com>
2019-04-11 21:24:43 +0800

    first

---
+++ src/lang/zh.js
@@ -0,0 +1,31 @@
+export default {
+  route: {
+    dashboard: '首页'
+  },
+  navbar: {
+    logOut: '退出登录',
+    dashboard: '首页',
+    theme: '换肤',
+    size: '布局大小'
+  },
+  login: {
+    title: '系统登录',
+    logIn: '登录',
+    username: '账号',
+    password: '密码'
+
+  },
+  tagsView: {
+    refresh: '刷新',
+    close: '关闭',
+    closeOthers: '关闭其它',
+    closeAll: '关闭所有'
+  },
+  settings: {
+    title: '系统布局配置',
+    theme: '主题色',
+    tagsView: '开启 Tags-View',
+    fixedHeader: '固定 Header',
+    sidebarLogo: '侧边栏 Logo'
+  }
+}
